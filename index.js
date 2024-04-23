de2f26d41e20a6e0fcb8d406b9b6755ef27586f9
Hernan Soberon <hernan.soberon@gmail.com>
2024-04-23 14:42:55 -0500

    Messages working

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ const corsOptions = {
     origin: "http://localhost:3000"
 };
 app.use(cors(corsOptions));
+
 // Set json for getting data from request body
 app.use(express.json());
+
 // Redis setup
 let redisClient;
 (async () => {
@@ -19,6 +21,7 @@ let redisClient;
     redisClient.on("connect", () => console.log("Redis connected"));
     await redisClient.connect();
 })();
+
 // MySQL setup
 const DB = mysql.createConnection({
     host    : 'localhost',
@@ -26,17 +29,21 @@ const DB = mysql.createConnection({
     password: 'root',
     database: 'now_chat',
 });
+
 // Connect to MySQL
 DB.connect((err) => {
     if (err) throw err;
     console.log('MySQL connected');
 });
+
 // Routes
 app.get('/', (req, res) => {
     res.send('<h1 style="text-align: center;">Welcome to the Now Chat api!</h1>');
 });
+
 // Start server
 const port = 5000;
+
 // Listen on port
 app.listen(port, () => {
     console.log(`Running at - http://localhost:${port}`);
@@ -178,6 +185,96 @@ app.delete('/users/:id', (req, res) => {
         return res.send({
             success: true,
             message: 'User deleted successfully!'
+        });
+    });
+});
+
+
+
+// >>>>>>>>>>> MESSAGES 
+
+// Fetching data from Database or Redis
+app.get('/messages/:firstUserId/:secondUserId', async (req, res) => {
+
+    const {firstUserId, secondUserId} = req.params;
+
+    const redisName = 'Messages_' + firstUserId + '_' + secondUserId;
+
+    try {
+        // Check if cached data exists in Redis or not. If yes, return cached data
+        const cachedData = await redisClient.get(redisName);
+        if (cachedData) {
+            return res.send({
+                success: true,
+                message: 'Messages retrieved from cache successfully!',
+                data   : JSON.parse(cachedData)
+            });
+        }
+
+        // If cached data doesn't exist, fetch data from database and cache it
+        const results = await new Promise((resolve, reject) => {
+            DB.query('SELECT * FROM `Messages` WHERE (from_id = ? and to_id = ?) OR (from_id = ? and to_id = ?) ORDER BY created', [firstUserId, secondUserId, secondUserId, firstUserId], (err, results) => {
+                if (err) reject(err);
+                resolve(results);
+            });
+        });
+
+        // If no data found in database, return error message
+        if (!results.length) {
+            return res.send({
+                success: false,
+                message: 'No Messages found!',
+                data   : results
+            });
+        }
+
+        // Cache data in Redis for 1 hour (3600 seconds)
+        redisClient.setEx(redisName, 3600, JSON.stringify(results));
+
+        // Return response
+        return res.send({
+            success: true,
+            message: 'Messages retrieved from database successfully!',
+            data   : results
+        });
+    } catch (error) { // Catch any error
+        throw error;
+    }
+});
+
+
+// Recieves a new message
+app.post('/messages', (req, res) => {
+    // Get data from request body
+    const {from_id, to_id, text} = req.body;
+
+    // Insert Message into database
+    DB.query('INSERT INTO Messages (from_id, to_id, message, created) VALUES (?, ?, ?, NOW())', [from_id, to_id, text], (err, results) => {
+        if (err) throw err; // Throw error if any
+
+        // If no rows affected, then Message not inserted
+        if (!results.affectedRows) {
+            return res.send({
+                success: false,
+                message: 'Message not recived!',
+                data   : results
+            });
+        }
+
+        // Delete cached data from bout Redis calls
+        redisClient.del('Messages_' + from_id + '_' + to_id);
+        redisClient.del('Messages_' + to_id + '_' + from_id);
+
+        // Return response
+        return res.send({
+            success: true,
+            message: 'Message recived!',
+            data   : {
+                id: results.insertId,
+                from_id,
+                to_id,
+                text,
+            }
         });
     });
 });
