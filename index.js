e061353199ee45c3474d4e7ef33210c436e4620c
Hernan Soberon <hernan.soberon@gmail.com>
2024-04-25 04:55:35 -0500

    remove http calls for messages

--- index.js
+++ index.js
@@ -72,10 +72,6 @@ wsServer.on("connection", (connection, request) => {
     }else{
         console.log("WS Connected");
     }
-    // getUsers().then(msg => {
-    //     console.log("Sending All Users");
-    //     connection.send(JSON.stringify(msg));
-    // });
 
     connection.on("message", (msg) => {
         const {type, message} = JSON.parse(msg);
@@ -96,6 +92,7 @@ wsServer.on("connection", (connection, request) => {
             }
         }
 
+        //Brodcast the message to all chats on that channel
         if(type == 'SEND Message'){
             const {userFrom, userTo} = JSON.parse(msg);
             newMessage(userFrom, userTo, message).then(text => {
@@ -207,16 +204,6 @@ async function getChat(from_id, to_id){
                 resolve(results);
             });
         });
-
-        // If no data found in database, return error message
-        // if (!results.length) {
-        //     return {
-        //         success: true,
-        //         type: 'Chat'
-        //         message: 'Empty Chat',
-        //         data   : results
-        //     };
-        // }
 
         // Cache data in Redis for 1 hour (3600 seconds)
         redisClient.setEx(redisName, 3600, JSON.stringify(results));
@@ -413,90 +400,3 @@ app.delete('/users/:id', (req, res) => {
 });
 
 
-
-
-// Fetching data from Database or Redis
-app.get('/messages/:firstUserId/:secondUserId', async (req, res) => {
-
-    const {firstUserId, secondUserId} = req.params;
-
-    const redisName = 'Messages_' + firstUserId + '_' + secondUserId;
-
-    try {
-        // Check if cached data exists in Redis or not. If yes, return cached data
-        const cachedData = await redisClient.get(redisName);
-        if (cachedData) {
-            return res.send({
-                success: true,
-                message: 'Messages retrieved from cache successfully!',
-                data   : JSON.parse(cachedData)
-            });
-        }
-
-        // If cached data doesn't exist, fetch data from database and cache it
-        const results = await new Promise((resolve, reject) => {
-            DB.query('SELECT * FROM `Messages` WHERE (from_id = ? and to_id = ?) OR (from_id = ? and to_id = ?) ORDER BY created', [firstUserId, secondUserId, secondUserId, firstUserId], (err, results) => {
-                if (err) reject(err);
-                resolve(results);
-            });
-        });
-
-        // If no data found in database, return error message
-        if (!results.length) {
-            return res.send({
-                success: false,
-                message: 'No Messages found!',
-                data   : results
-            });
-        }
-
-        // Cache data in Redis for 1 hour (3600 seconds)
-        redisClient.setEx(redisName, 3600, JSON.stringify(results));
-
-        // Return response
-        return res.send({
-            success: true,
-            message: 'Messages retrieved from database successfully!',
-            data   : results
-        });
-    } catch (error) { // Catch any error
-        throw error;
-    }
-});
-
-
-// Recieves a new message
-app.post('/messages', (req, res) => {
-    // Get data from request body
-    const {from_id, to_id, text} = req.body;
-
-    // Insert Message into database
-    DB.query('INSERT INTO Messages (from_id, to_id, message, created) VALUES (?, ?, ?, NOW())', [from_id, to_id, text], (err, results) => {
-        if (err) throw err; // Throw error if any
-
-        // If no rows affected, then Message not inserted
-        if (!results.affectedRows) {
-            return res.send({
-                success: false,
-                message: 'Message not recived!',
-                data   : results
-            });
-        }
-
-        // Delete cached data from bout Redis calls
-        redisClient.del('Messages_' + from_id + '_' + to_id);
-        redisClient.del('Messages_' + to_id + '_' + from_id);
-
-        // Return response
-        return res.send({
-            success: true,
-            message: 'Message recived!',
-            data   : {
-                id: results.insertId,
-                from_id,
-                to_id,
-                text,
-            }
-        });
-    });
-});
